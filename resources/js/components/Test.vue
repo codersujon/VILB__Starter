aa3ed9cf3d72f2ea24cd34c0b783df26b22684cb
unknown <csesujon155@gmail.com>
2024-09-25 13:20:26 -0700

    api response done

--- resources/js/components/Test.vue
+++ resources/js/components/Test.vue
@@ -1,8 +1,20 @@
 <script setup>
+    import axios from 'axios'
+    import { ref } from 'vue'
 
+    let Data = ref("");
+    // API Data Access
+    try {
+        axios.get('http://127.0.0.1:8000/api/test').then(response =>{
+            Data.value = response.data;
+        });
+    } catch (error) {
+        console.log(error);
+    }
+        
 </script>
 <template>
     <div>
-        <h1 class="display-1 text-center text-primary">VLB Starter Pack</h1>
+    <h1 class="display-2 text-center text-primary py-5">{{ Data }}</h1>
     </div>
 </template>
