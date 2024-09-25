6b74dfcc2a9c34cf9bf60926da0e653ea7599e2e
unknown <csesujon155@gmail.com>
2024-09-25 12:28:06 -0700

    Laravel UI Install and Test Component Create

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,11 @@
 import './bootstrap';
+import { createApp } from 'vue';
+
+const app = createApp({});
+
+import TestComponent from './components/Test.vue';
+app.component(
+    'test-component', TestComponent
+);
+
+app.mount('#app')
---
+++ resources/js/components/Test.vue
@@ -0,0 +1,8 @@
+<script setup>
+
+</script>
+<template>
+    <div>
+        <h1 class="display-1 text-center text-primary">VLB Starter Pack</h1>
+    </div>
+</template>
